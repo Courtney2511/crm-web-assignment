8af827d7d7346dd943436a5bc87a7e9a876152e3
Courtney Noonan <courtneynoonan@me.com>
2016-06-08 16:23:15 -0400

    4. Implement View all Contacts completed

--- crm.rb
+++ crm.rb
@@ -2,9 +2,14 @@ require_relative 'contact'
 require 'sinatra'
 
 get '/' do
+  @crm_app_name = "Bitmaker's CRM"
   erb :index
 end
 
 get '/contacts' do
-  
+  Contact.create('Mark', 'Zuckerberg', 'mark@facebook.com', 'CEO')
+  Contact.create('Sergey', 'Brin', 'sergey@google.com', 'Co-Founder')
+  Contact.create('Steve', 'Jobs', 'steve@apple.com', 'Visionary')
+
+  erb :contacts
 end
