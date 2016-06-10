e93a1ae24b7a699f66b0628849b9b88361ac8fe5
Courtney Noonan <courtneynoonan@me.com>
2016-06-10 15:03:45 -0400

    added edit link

--- crm.rb
+++ crm.rb
@@ -1,12 +1,6 @@
 require_relative 'contact'
 require 'sinatra'
 
-# Temporary fake data so that we always find contact with id 1.
-Contact.create('Johnny', 'Bravo', 'johnny@bitmakerlabs.com', 'Rockstar')
-Contact.create('Courtney', 'Noonan', 'courtney@bitmakerlabs.com', 'Rockstar')
-Contact.create('James', 'Bibby', 'jamie@bitmakerlabs.com', 'Rockstar')
-Contact.create('Owen', 'Noonan', 'owen@bitmakerlabs.com', 'Rockstar')
-
 get '/' do
   @crm_app_name = "Bitmaker's CRM"
   erb :index
